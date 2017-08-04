8a6b6bcceb4b8f9761e8869fb868bc0fd0787c01
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2017-08-04 14:59:10 +0200

    Added some benchmarks.

--- bench/Main.elm
+++ bench/Main.elm
@@ -30,16 +30,22 @@ suite n =
             [ Benchmark.compare "Singleton"
                 (benchmark2 "Original" Dict.singleton n n)
                 (benchmark2 "Updated" Dict2.singleton n n)
+            , Benchmark.compare "Size"
+                (benchmark1 "Original" Dict.size original)
+                (benchmark1 "Updated" Dict2.size updated)
             , Benchmark.compare "Filter"
                 (benchmark2 "Original" Dict.filter (\_ v -> v < halfwayPoint) original)
                 (benchmark2 "Updated" Dict2.filter (\_ v -> v < halfwayPoint) updated)
-            , Benchmark.compare "Partition"
-                (benchmark2 "Original" Dict.partition (\_ v -> v < halfwayPoint) original)
-                (benchmark2 "Updated" Dict2.partition (\_ v -> v < halfwayPoint) updated)
             , Benchmark.compare "Insert"
                 (benchmark3 "Original" Dict.insert -1 -1 original)
                 (benchmark3 "Updated" Dict2.insert -1 -1 updated)
+            , Benchmark.compare "Update insert"
+                (benchmark3 "Original" Dict.update -1 (always <| Just -5) original)
+                (benchmark3 "Update" Dict2.update -1 (always <| Just -5) updated)
             , Benchmark.compare "Remove"
                 (benchmark2 "Original" Dict.remove 0 original)
                 (benchmark2 "Updated" Dict2.remove 0 updated)
+            , Benchmark.compare "Update remove"
+                (benchmark3 "Original" Dict.update -1 (always Nothing) original)
+                (benchmark3 "Update" Dict2.update -1 (always Nothing) updated)
             ]
